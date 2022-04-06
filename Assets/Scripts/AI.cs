5092720c58df6469d0b98dc3fc1c51b85dc0771d
Joe-CDN <35740892+Joe-CDN@users.noreply.github.com>
2022-04-06 18:33:58 -0400

    added speech bubbles

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -9,6 +9,14 @@ public class AI : MonoBehaviour
     public GameObject EndPos;
     public GameObject hand;
     public GameObject bubble;
+    public ParticleSystem faeBubble;
+    public ParticleSystem goldBubble;
+    public ParticleSystem happyBubble;
+    public ParticleSystem loveBubble;
+    public ParticleSystem pureBubble;
+    public ParticleSystem sadBubble;
+    public ParticleSystem timeBubble;
+    public ParticleSystem truthBubble;
 
     [SerializeField]    private GameObject bloodPrefab;
     
@@ -38,6 +46,10 @@ public class AI : MonoBehaviour
                 Vector3 interpolatedPosition = Vector3.Lerp(StartPos.transform.position, EndPos.transform.position, PersistanceManager.instance.npcApproachPercent);
 
                 this.transform.position = interpolatedPosition;
+                if (PersistanceManager.instance.npcApproachPercent >= 1)
+                {
+                    RequestPotion();
+                }
                 if (PersistanceManager.instance.npcApproachPercent >= 1 && potionTotalRequested == 50.61f) {
                     giveDNA();
                 }
@@ -55,7 +67,6 @@ public class AI : MonoBehaviour
 
                 if (PersistanceManager.instance.npcApproachPercent >= 1) {
                     PersistanceManager.instance.npcApproach = true;
-                    RequestPotion();                    
                     this.transform.rotation = Quaternion.Euler(0f, -90f, 0f);
                     PersistanceManager.instance.npcApproachPercent = 0;
                 }
@@ -118,7 +129,15 @@ public class AI : MonoBehaviour
     {
         Destroy(collision.gameObject);
         PersistanceManager.instance.npcApproachPercent = 0;
-        PersistanceManager.instance.npcApproach = false;            
+        PersistanceManager.instance.npcApproach = false;
+        pureBubble.gameObject.SetActive(false);
+        goldBubble.gameObject.SetActive(false);
+        sadBubble.gameObject.SetActive(false);
+        loveBubble.gameObject.SetActive(false);
+        faeBubble.gameObject.SetActive(false);
+        timeBubble.gameObject.SetActive(false);
+        truthBubble.gameObject.SetActive(false);
+        happyBubble.gameObject.SetActive(false);
     }
     void RequestPotion()
     {
@@ -126,35 +145,43 @@ public class AI : MonoBehaviour
 
         if(randPotRequest == 0){
             potionTotalRequested = 27.77f; //purify potion
-            bubble.GetComponentInChildren<MeshRenderer>().material.color = new Color(0.6509434f, 0.6349769f, 0.6349769f);
+            //bubble.GetComponentInChildren<MeshRenderer>().material.color = new Color(0.6509434f, 0.6349769f, 0.6349769f);
+            pureBubble.gameObject.SetActive(true);
         }
         if(randPotRequest == 1){
             potionTotalRequested = 23.26f; //gold potion
-            bubble.GetComponentInChildren<MeshRenderer>().material.color = new Color(1f, 0.8382407f, 0f);
+            //bubble.GetComponentInChildren<MeshRenderer>().material.color = new Color(1f, 0.8382407f, 0f);
+            goldBubble.gameObject.SetActive(true);
         }
         if(randPotRequest == 2){
             potionTotalRequested = 29.18f; //sad potion
-            bubble.GetComponentInChildren<MeshRenderer>().material.color = new Color(0.1437f, 0.01331432f, 0.4150943f);
+            //bubble.GetComponentInChildren<MeshRenderer>().material.color = new Color(0.1437f, 0.01331432f, 0.4150943f);
+            sadBubble.gameObject.SetActive(true);
         }
         if(randPotRequest == 3){
             potionTotalRequested = 50.61f; // love potion
-            bubble.GetComponentInChildren<MeshRenderer>().material.color = new Color(1f, 0f, 0.9095554f);
+            //bubble.GetComponentInChildren<MeshRenderer>().material.color = new Color(1f, 0f, 0.9095554f);
+            loveBubble.gameObject.SetActive(true);
         }
         if(randPotRequest == 4){
             potionTotalRequested = 30.21f; //cat fairy potion
-            bubble.GetComponentInChildren<MeshRenderer>().material.color = new Color(0f, 1f, 0.7181935f);
+            //bubble.GetComponentInChildren<MeshRenderer>().material.color = new Color(0f, 1f, 0.7181935f);
+            faeBubble.gameObject.SetActive(true);
         }
         if(randPotRequest == 5){
             potionTotalRequested = 26.19f; //time potion
-            bubble.GetComponentInChildren<MeshRenderer>().material.color = new Color(0f, 0.4150943f, 0.05119479f);
+            //bubble.GetComponentInChildren<MeshRenderer>().material.color = new Color(0f, 0.4150943f, 0.05119479f);
+            timeBubble.gameObject.SetActive(true);
         }
         if(randPotRequest == 6){
             potionTotalRequested = 14.88f; //truth potion
-            bubble.GetComponentInChildren<MeshRenderer>().material.color = new Color(0f, 0.5359084f, 1f);
+            //bubble.GetComponentInChildren<MeshRenderer>().material.color = new Color(0f, 0.5359084f, 1f);
+            truthBubble.gameObject.SetActive(true);
         }
         if(randPotRequest == 7){
             potionTotalRequested = 23.42f; //happy potion
-            bubble.GetComponentInChildren<MeshRenderer>().material.color = new Color(1f, 0.4665077f, 0f);
+            //bubble.GetComponentInChildren<MeshRenderer>().material.color = new Color(1f, 0.4665077f, 0f);
+            happyBubble.gameObject.SetActive(true);
         }
     }
 
